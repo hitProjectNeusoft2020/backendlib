c42d5674f68b946832a90b49e3e9b100ee964439
1170300405 <1151906899@qq.com>
2020-07-27 09:35:27 +0800

    项目第一次提交

---
+++ HMS005/src/main/java/com/neusoft/hotel/Hms005Application.java
@@ -0,0 +1,13 @@
+package com.neusoft.hotel;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Hms005Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Hms005Application.class, args);
+	}
+
+}
---
+++ HMS005/src/test/java/com/neusoft/hotel/Hms005ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.neusoft.hotel;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Hms005ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
